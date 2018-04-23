a809ecc031b3846bc58d9960a2bfc5d7fcb0d27b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to customize Search Panel by using the LayoutHelper class
+
+
+<p>Generally, WPF controls can be customized via <a href="http://www.wpftutorial.net/templates.html"><u>Control Templates</u></a>. Although this customization method has many advantages, sometimes you just wish to get a quick solution without writing additional XAML. This example illustrates how to customize Search Panel of the RichEditControl (see the image below) programmatically. The approach demonstrated in this example implies the use of a very handy <strong>LayoutHelper </strong>class. It allows you to search FrameworkElements by the specified criteria and can be very helpful in some situations where the only alternative is to write a large style/template. Note the way in which <a href="http://msdn.microsoft.com/en-us/library/system.windows.uielement.layoutupdated.aspx"><u>UIElement.LayoutUpdated Event</u></a> is utilized in this example.</p><p><img src="https://raw.githubusercontent.com/DevExpress-Examples/how-to-customize-search-panel-by-using-the-layouthelper-class-e4008/11.2.11+/media/0efe1023-cf58-4f3c-ae81-18579c37367a.png"></p>
+
+<br/>
+
+
---
+++ VB/MainWindow.xaml.vb
@@ -0,0 +1,36 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Windows
+Imports System.Windows.Controls
+Imports DevExpress.Xpf.Core.Native
+Imports DevExpress.Xpf.Editors
+Imports DevExpress.Xpf.RichEdit.UI
+Imports DevExpress.XtraRichEdit.Commands
+
+Namespace RichEditSearchPanelWpf
+	Partial Public Class MainWindow
+		Inherits Window
+		Private searchPanel As RichEditSearchPanel
+
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub richEditControl1_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			richEditControl1.CreateCommand(RichEditCommandId.Find).Execute()
+			searchPanel = CType(LayoutHelper.FindElementByName(richEditControl1, "SearchPanel"), RichEditSearchPanel)
+			AddHandler searchPanel.LayoutUpdated, AddressOf searchPanel_LayoutUpdated
+		End Sub
+
+		Private Sub searchPanel_LayoutUpdated(ByVal sender As Object, ByVal e As EventArgs)
+            Dim lbPrompt As TextBlock = TryCast(LayoutHelper.FindElement(searchPanel, Function(element) TypeOf element Is TextBlock AndAlso (CType(element, TextBlock)).Text.Contains("Find")), TextBlock)
+			Dim tbSearchString As ButtonEdit = CType(LayoutHelper.FindElementByName(searchPanel, "tbSearchString"), ButtonEdit)
+
+			If lbPrompt IsNot Nothing AndAlso tbSearchString IsNot Nothing Then
+				lbPrompt.FontSize = 16
+				tbSearchString.EditValue = "test"
+				RemoveHandler searchPanel.LayoutUpdated, AddressOf searchPanel_LayoutUpdated
+			End If
+		End Sub
+	End Class
+End Namespace
